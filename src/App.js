aa1d12625098fdbd1dd894e3befe321bae66b2d1
Carlos Ceron <carlos.ceron.1986@gmail.com>
2023-05-05 19:34:54 -0500

    Qr Component finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import qrImage from './assets/images/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Qr-container">
+      <img src={qrImage} className="Qr-image" alt="qr-code-component" />
+      <p className='Qr-title'>Improve your front-end skills by building projects</p>
+      <p className='Qr-subtitle'>Scan the QR code to visit Frontend Mentor and take your  coding skills to the next level</p>
     </div>
   );
 }
